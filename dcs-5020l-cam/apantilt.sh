ec5b7f5f44a80cb2cba96ff35ec544433e35dbae
kolwin bandara <kolwin@vendee.com>
2017-12-15 12:29:04 +0530

    changed

--- dcs-5020l-cam/apantilt.sh
+++ dcs-5020l-cam/apantilt.sh
@@ -15,7 +15,7 @@ $'\x1b[A' ) # up
 index=1
 while [ $index -lt 80 ]
 do
-curl --user admin:k1o2l3w4i5n6 --data "PanSingleMoveDegree=1&TiltSingleMoveDegree=1&PanTiltSingleMove=1" http://192.168.0.20/pantiltcontrol.cgi
+curl --user admin:password --data "PanSingleMoveDegree=1&TiltSingleMoveDegree=1&PanTiltSingleMove=1" http://192.168.0.20/pantiltcontrol.cgi
 let index++
 #sleep 1
 done;;
@@ -25,7 +25,7 @@ $'\x1b[B' ) #down
 index=1
 while [ $index -gt -30 ]
 do
-curl --user admin:k1o2l3w4i5n6 --data "PanSingleMoveDegree=7&TiltSingleMoveDegree=7&PanTiltSingleMove=7" http://192.168.0.20/pantiltcontrol.cgi
+curl --user admin:password --data "PanSingleMoveDegree=7&TiltSingleMoveDegree=7&PanTiltSingleMove=7" http://192.168.0.20/pantiltcontrol.cgi
 let index++
 #sleep 1
 done;;
@@ -34,7 +34,7 @@ $'\x1b[D' ) #left
 index=1
 while [ $index -gt -170 ]
 do
-curl --user admin:k1o2l3w4i5n6 --data "PanSingleMoveDegree=3&TiltSingleMoveDegree=3&PanTiltSingleMove=3" http://192.168.0.20/pantiltcontrol.cgi
+curl --user admin:password --data "PanSingleMoveDegree=3&TiltSingleMoveDegree=3&PanTiltSingleMove=3" http://192.168.0.20/pantiltcontrol.cgi
 let index++
 #sleep 1
 done;;
@@ -43,14 +43,14 @@ $'\x1b[C' ) # right
 index=1
 while [ $index -lt 170 ]
 do
-curl --user admin:k1o2l3w4i5n6 --data "PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=5" http://192.168.0.20/pantiltcontrol.cgi
+curl --user admin:password --data "PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=5" http://192.168.0.20/pantiltcontrol.cgi
 let index++
 #sleep 1
 done;;
 
 
 $'\eOH' ) #home
-curl --user admin:k1o2l3w4i5n6 --data "PanSingleMoveDegree=4&TiltSingleMoveDegree=4&PanTiltSingleMove=4" http://192.168.0.20/pantiltcontrol.cgi;;
+curl --user admin:password --data "PanSingleMoveDegree=4&TiltSingleMoveDegree=4&PanTiltSingleMove=4" http://192.168.0.20/pantiltcontrol.cgi;;
 
 * ) autopantilt;;
 #fi
--- dcs-5020l-cam/daynight.sh
+++ dcs-5020l-cam/daynight.sh
@@ -3,7 +3,7 @@ read -s -n1 key # read 3 characters
 
 case $key in
 a ) 
-curl --user admin:k1o2l3w4i5n6 --data "DayNightMode=2" http://192.168.0.20/cgi/daynightmode.cgi
+curl --user admin:password --data "DayNightMode=2" http://192.168.0.20/cgi/daynightmode.cgi
 echo "successfull"
 ;;
 esac
